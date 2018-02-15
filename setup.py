176ca1037a13d1e1316038bb6efcc32f11e3d01a
JulienGrv <grave.jul@gmail.com>
2018-02-15 14:09:31 +0100

    style(setup): use 2 spaces indentation

--- setup.py
+++ setup.py
@@ -5,42 +5,42 @@ from setuptools import setup
 
 
 def restify():
-    if os.path.isfile('README.md'):
-        if os.system('pandoc -s README.md -o README.rst') != 0:
-            print('----------------------------------------------------------')
-            print(
-                'WARNING: pandoc command failed, could not restify README.md')
-            print('----------------------------------------------------------')
-            if sys.stdout.isatty():
-                if sys.version_info[0] >= 3:
-                    input("Enter to continue... ")
-                else:
-                    raw_input("Enter to continue... ")
+  if os.path.isfile('README.md'):
+    if os.system('pandoc -s README.md -o README.rst') != 0:
+      print('----------------------------------------------------------')
+      print(
+        'WARNING: pandoc command failed, could not restify README.md')
+      print('----------------------------------------------------------')
+      if sys.stdout.isatty():
+        if sys.version_info[0] >= 3:
+          input("Enter to continue... ")
         else:
-            with open('README.rst') as fp:
-                return fp.read()
+          raw_input("Enter to continue... ")
+    else:
+      with open('README.rst') as fp:
+        return fp.read()
 
 
 setup(
-    name="localimport",
-    version="1.5.2",
-    description="Isolated import of Python Modules",
-    long_description=restify(),
-    author="Niklas Rosenstein",
-    author_email="rosensteinniklas@gmail.com",
-    py_modules=["localimport"],
-    keywords=["import", "embedded", "modules", "packages"],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Other Environment', 'Environment :: Plugins',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: Jython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities'
-    ])
+  name="localimport",
+  version="1.5.2",
+  description="Isolated import of Python Modules",
+  long_description=restify(),
+  author="Niklas Rosenstein",
+  author_email="rosensteinniklas@gmail.com",
+  py_modules=["localimport"],
+  keywords=["import", "embedded", "modules", "packages"],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Other Environment', 'Environment :: Plugins',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: Jython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Utilities'
+  ])
